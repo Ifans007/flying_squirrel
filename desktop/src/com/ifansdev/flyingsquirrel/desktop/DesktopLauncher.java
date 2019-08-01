f945eac5904b58cbee8be0962d31680822fa3fb4
Evgeniy Berezovskiy <Ifans007@mail.ru>
2019-08-01 15:47:58 +0300

    first commit

---
+++ desktop/src/com/ifansdev/flyingsquirrel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ifansdev.flyingsquirrel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ifansdev.flyingsquirrel.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
